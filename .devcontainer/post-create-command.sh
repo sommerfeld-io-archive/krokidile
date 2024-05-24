08ca9ac6b25534769624ea1f86c900c3d3ceeef2
Sebastian Sommerfeld <sebatian@sommerfeld.io>
2024-05-24 13:37:03 +0000

    cleanup

---
+++ .devcontainer/post-create-command.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+## Install tools and dependencies for the project.
+## This script is run after the devcontainer is created.
+##
+## @see .devcontainer/devcontainer.json
+
+echo "[INFO] Install and initialize husky"
+# https://dev.to/ivadyhabimana/setup-eslint-prettier-and-husky-in-a-node-project-a-step-by-step-guide-946
+# npm install eslint --save-dev
+# yes | npx husky init
