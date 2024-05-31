947e12914678629e6ff446f0fcd0c142a6d86baa
Sebastian Sommerfeld <sebatian@sommerfeld.io>
2024-05-31 19:49:28 +0000

    ci: lint commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'test']],
-        'subject-full-stop': [0, 'never'],
-        'subject-case': [0, 'never']
-    }
+    extends: ['@commitlint/config-conventional']
 };
