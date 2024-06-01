a0033dee42b9d2bebaddee29c1da86ff7f603470
Sebastian Sommerfeld <sebatian@sommerfeld.io>
2024-06-01 21:04:11 +0000

    style: basic vanilla layout

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './styles.scss';
 
 function App() {
-  return <h1>I will be a plantuml live editor</h1>;
+  return <p>I will be a plantuml live editor</p>;
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
