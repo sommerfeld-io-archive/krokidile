030ec8531f6a342b074419c4c309c3f8a08c14ff
Sebastian Sommerfeld <sebatian@sommerfeld.io>
2024-06-03 19:25:57 +0000

    style: introduce bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return <p>I will be a plantuml live editor</p>;
 }
