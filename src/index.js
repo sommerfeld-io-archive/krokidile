f0fc028649846060824ec42deec2cf71b4959c50
Sebastian Sommerfeld <sebatian@sommerfeld.io>
2024-06-04 20:16:54 +0000

    feat: send requests to kroki.oio (work in progress)

--- src/index.js
+++ src/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+// todo https://github.com/sommerfeld-io/krokidile/issues/41  
+const krokiUrl = "https://kroki.io";
+
+document.getElementById('diagram-code').addEventListener('input', async function(e) {
+  const diagramCode = e.target.value;
+  
+  if (!diagramCode) {
+    console.error('Diagram code is empty');
+    return;
+  }
+
+  const encodedDiagramCode = btoa(encodeURIComponent(diagramCode));
+
+  const response = await fetch(`${krokiUrl}/plantuml/svg/${encodedDiagramCode}`);
+  const imageUrl = await response.text();
+  document.getElementById('preview').innerHTML = `<img src="${imageUrl}" alt="Diagram Preview" />`;
+});
+
+function KrokiUrl() {
   return (
-    <div>
-      <p>I will be a plantuml live editor</p>
-    </div>
+    <a href={krokiUrl}>
+      {krokiUrl}
+    </a>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('editor'));
+ReactDOM.render(<KrokiUrl />, document.getElementById('kroki-url'));
