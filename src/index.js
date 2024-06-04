6bcf2888c84758721c29aa5bd6458e2a31725487
Sebastian Sommerfeld <sebatian@sommerfeld.io>
2024-06-04 20:17:21 +0000

    feat: send requests to kroki.oio (work in progress)

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// todo https://github.com/sommerfeld-io/krokidile/issues/41  
-const krokiUrl = "https://kroki.io";
+// todo https://github.com/sommerfeld-io/krokidile/issues/41
+const krokiUrl = 'https://kroki.io';
 
-document.getElementById('diagram-code').addEventListener('input', async function(e) {
+document.getElementById('diagram-code').addEventListener('input', async function (e) {
   const diagramCode = e.target.value;
-  
+
   if (!diagramCode) {
     console.error('Diagram code is empty');
     return;
@@ -22,11 +22,7 @@ document.getElementById('diagram-code').addEventListener('input', async function
 });
 
 function KrokiUrl() {
-  return (
-    <a href={krokiUrl}>
-      {krokiUrl}
-    </a>
-  );
+  return <a href={krokiUrl}>{krokiUrl}</a>;
 }
 
 ReactDOM.render(<KrokiUrl />, document.getElementById('kroki-url'));
