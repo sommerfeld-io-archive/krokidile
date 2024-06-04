5e884ea80740db85f9ed28ef25647f4e2eda6011
Sebastian Sommerfeld <sebatian@sommerfeld.io>
2024-06-04 20:31:47 +0000

    style: update top navigation and introduce  dark mode

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ document.getElementById('diagram-code').addEventListener('input', async function
 });
 
 function KrokiUrl() {
-  return <a href={krokiUrl}>{krokiUrl}</a>;
+  return <a href={krokiUrl} class="nav-link link-body-emphasis px-2">{krokiUrl}</a>;
 }
 
 ReactDOM.render(<KrokiUrl />, document.getElementById('kroki-url'));
